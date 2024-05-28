4bbc30359dbbe7a22889f7ed49b9f39bef734172
Adam Parsons <ParsonsA10@cardiff.ac.uk>
2024-05-28 09:23:32 +0100

    added a way to get the file name inside the function

--- Audio length generator.py
+++ Audio length generator.py
@@ -1,13 +1,19 @@
 from mutagen.mp3 import MP3
+import os
 # function to convert the information into 
 # some readable format 
-def audio_duration(file_name): 
-	audio = MP3(file_name)
+def audio_duration(file_path): 
+	
+	file_name = os.path.basename(file_path)
+	file = os.path.splitext(file_name)
+	name = file [0]# returns the name of the file
+
+	audio = MP3(file_path)
 	audio_info = audio.info 
 	length = int(audio_info.length) 
 	mins = length // 60 # calculate in minutes 
 	if length > 0:
 		mins += 1
-	return mins # returns the duration 
+	return name, mins # returns the duration 
 
 print(audio_duration("E16 - Chase the Night.mp3"))
