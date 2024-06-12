33ddce18ae0f28fdf0486f8ad0d9f14e12633fc4
Adam Parsons <ParsonsA10@cardiff.ac.uk>
2024-06-12 16:58:03 +0100

    adds to sheet, is done

--- Audio length generator.py
+++ Audio length generator.py
@@ -1,6 +1,11 @@
 from mutagen.mp3 import MP3
 import os
 import glob
+from openpyxl import load_workbook
+
+spreadsheet= "audio time spreadsheet.xlsx"
+
+
 # function to convert the information into 
 # some readable format 
 def file_name(file_path):
@@ -9,8 +14,6 @@ def file_name(file_path):
 	name = file [0]# returns the name of the file
 	
 	return name
-
-
 
 def audio_duration(file_path): 
 	
@@ -22,9 +25,17 @@ def audio_duration(file_path):
 	mins = length // 60 # calculate in minutes 
 	if length > 0:
 		mins += 1
-	return name, mins # returns the duration 
+	return [name, mins] # returns the duration 
 
-#print(audio_duration("C:/Users/adama/Downloads/Big Finish Productions/1. Doctor Who/5. Bernice Summerfield/2. Bernice Summerfield Audiobooks (BSAB)/BSAB 1.0X - Treasury.mp3"))
+def addtospreadsheet(new_data):
+	work_book = load_workbook(spreadsheet)
+	work_sheet = work_book.active
+	
+	for row in new_data:
+		work_sheet.append(row)
+	
+	work_book.save(spreadsheet)
+
 
 mypath = ("C:/Users/adama/Downloads/Big Finish Productions/*")
 
@@ -33,18 +44,17 @@ while True:
 	for item in everyitem:
 		#print(item)
 		if ".mp3" in item :
-			print (audio_duration(item))
-			
-		
+			addtospreadsheet([audio_duration(item)])#get name and time of mp3 files and add to sheet	
 		else:
-			#print ('false ' + item)
 			mypath+=("/*")
 			everyitem = glob.glob(mypath)
 			
 			if ".mp3" not in item:
 				try: 
-					print (file_name(item))
+					data_to_add = (file_name(item))
+					if data_to_add.isnumeric() == False:
+						new_data = [[data_to_add,"N/A"]]
+						addtospreadsheet(new_data)
 				except:	
-					print(item)
-			#print (everyitem)
+					break
 
